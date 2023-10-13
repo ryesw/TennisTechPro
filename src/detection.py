643c45e30455936df9a7cb14dc7a81efff609d69
rye <tjddnrla0@gmail.com>
2023-10-13 22:53:54 +0900

    improve saving box coordinates

--- src/detection.py
+++ src/detection.py
@@ -7,21 +7,17 @@ class DetectionModel:
         self.detection_model = detection_model
         self.match_type = match_type # 단식 2명, 복식 4명
         self.dtype = dtype
-        self.PLAYER_1_LABEL = 0
-        self.PLAYER_2_LABEL = 1
-        self.PLAYER_3_LABEL = 2
-        self.PLAYER_4_LABEL = 3
-        self.RACKET_LABEL = 43
-        self.BALL_LABEL = 37
-        self.PERSON_SCORE_MIN = 0.85
-        self.PERSON_SECONDARY_SCORE = 0.3
-        self.RACKET_SCORE_MIN = 0.6
-        self.BALL_SCORE_MIN = 0.6
-        self.v_width = 0 # video_width
-        self.v_height = 0 # video_height
+        self.BOTTOM_PLAYER_1 = 0
+        self.BOTTOM_PLAYER_2 = 2
+        self.TOP_PLAYER_1 = 1
+        self.TOP_PLAYER_2 = 3
+        self.BOTTOM_MIN_CONF = 0.55 # Bottom 선수 Detection 시 최소 confidence 값
+        self.TOP_MIN_CONF = 0.32 # Top 선수 Detection 시 최소 confidence 값
         self.line_thickness = 2
-        self.player_1_boxes = []
-        self.player_2_boxes = []
+        self.__player_1_boxes = []
+        self.__player_2_boxes = []
+        self.__player_1_count = 0
+        self.__player_2_count = 0
 
 
     def detect(self, image, person_min_score=None):
@@ -29,34 +25,68 @@ class DetectionModel:
         Use YOLO Model to detect players in the one frame
         """
         results = self.detection_model.predict(image)
-        count = 0
+
         for result in results:
             annotator = Annotator(image, line_width=self.line_thickness, font_size=None)
 
             boxes = result.boxes
-            # # Detection Model의 정확도가 높으면 이렇게 해도 OK
+            
+            for i in range(len(boxes.xyxy)):
+                box = boxes.xyxy[i].cpu().numpy()
+                cls = boxes.cls[i].cpu().numpy()
+                conf = boxes.conf[i].cpu().numpy()
+                
+                if cls == 0: # if cls == 0 and conf >= self.BOTTOM_MIN_CONF:
+                    if len(self.__player_1_boxes) == 0:
+                        self.__player_1_boxes.append(box)
+                        self.__player_1_count += 1
+                elif cls == 1: # elif cls == 1 and conf >= self.TOP_MIN_CONF:
+                    if len(self.__player_2_boxes) == 0:
+                        self.__player_2_boxes.append(box)
+                        self.__player_2_count += 1
+
+                annotator.box_label(box, label=False, color=(255, 0, 255))
+            # 1. Detection Model의 정확도가 높을 때
             # for box in boxes:
             #     cls = int(box.cls.cpu().numpy()) # class 정보: bottom=0, top=1
             #     pts = box.xyxy[0].cpu().numpy() # box 좌표
+            #     conf = box.conf[0].cpu().numpy() # confidence 값
+                
+            #     if cls == 0 and conf >= self.BOTTOM_MIN_CONF:
+            #         self.__player_1_boxes.append(pts)
+            #     elif cls == 1 and conf >= self.TOP_MIN_CONF:
+            #         self.__player_2_boxes.append(pts)
 
-            #     if cls == 0:
-            #         self.player_1_boxes.append(pts)
-            #     elif cls == 1:
-            #         self.player_2_boxes.append(pts)
-            for idx, box in enumerate(boxes):
-                cls = int(box.cls.cpu().numpy()) # class 정보: bottom=0, top=1
-                pts = box.xyxy[0].cpu().numpy() # box 좌표
+            # 2. 최초의 2개만 
+            # for idx, box in enumerate(boxes):
+            #     cls = int(box.cls.cpu().numpy()) # class 정보: bottom=0, top=1
+            #     pts = box.xyxy[0].cpu().numpy() # box 좌표
 
-                if idx == 0:
-                    self.player_1_boxes.append(pts)
-                elif idx == 1:
-                    self.player_2_boxes.append(pts)
+            #     if idx == 0:
+            #         self.__player_1_boxes.append(pts)
+            #     elif idx == 1:
+            #         self.__player_2_boxes.append(pts)
 
-                # annotator.box_label(pts, self.detection_model.names[cls]) -> Label까지 나오도록 하는 Code
-                annotator.box_label(pts, label=False, color=(255, 0, 0))
+                    # annotator.box_label(pts, self.detection_model.names[cls]) -> Label까지 나오도록 하는 Code
+                    # annotator.box_label(box, label=False, color=(0, 0, 0))
 
         frame = annotator.result()
         return frame
+    
+
+    def initialize_box(self):
+        self.__player_1_boxes = []
+        self.__player_2_boxes = []
+
+
+    def get_boxes(self):
+        return self.__player_1_boxes, self.__player_2_boxes
+    
+
+    def print_counts(self):
+        print('Player 1 Object Detection Count: ', self.__player_1_count)
+        print('Player 2 Object Detection Count: ', self.__player_2_count)
+    
     
 def center_of_box(box):
     """
