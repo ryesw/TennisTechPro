8e4532f787534f95b6c3749db2625a50f7545114
rye <tjddnrla0@gmail.com>
2023-10-13 22:54:47 +0900

    check other models

--- src/yolo.py
+++ src/yolo.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 
 detection_model = YOLO('yolo/best_detection.pt')
+# detection_model = YOLO('yolo/best10.pt') -> 성능 좋지 않음
+# detection_model = YOLO('yolo/best1-10.pt') -> 성능 좋지 않음
 pose_model = YOLO('yolo/yolov8x-pose-p6.pt')
 thetis_model = YOLO('yolo/yolov8x-pose.pt')
