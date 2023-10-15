53a78cdafb1a24acb4a3cbc6d4c7445ee3f3b0aa
rye <tjddnrla0@gmail.com>
2023-10-15 14:22:17 +0900

    modify thetis model

--- src/yolo.py
+++ src/yolo.py
@@ -4,4 +4,4 @@ detection_model = YOLO('yolo/best_detection.pt')
 # detection_model = YOLO('yolo/best10.pt') -> 성능 좋지 않음
 # detection_model = YOLO('yolo/best1-10.pt') -> 성능 좋지 않음
 pose_model = YOLO('yolo/yolov8x-pose-p6.pt')
-thetis_model = YOLO('yolo/yolov8x-pose.pt')
+thetis_model = YOLO('yolo/yolov8x-pose-p6.pt')
